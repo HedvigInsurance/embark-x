60552e3bd52a8d2930731a73e6aaac3b44ad3750
stylianosgakis <stylianos.gakis98@gmail.com>
2022-02-21 11:09:08 +0200

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/hedvig/embarkx/Platform.kt
@@ -0,0 +1,5 @@
+package com.hedvig.embarkx
+
+expect class Platform() {
+    val platform: String
+}
---
+++ shared/src/iosTest/kotlin/com/hedvig/embarkx/iosTest.kt
@@ -0,0 +1,12 @@
+package com.hedvig.embarkx
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class IosGreetingTest {
+
+    @Test
+    fun testExample() {
+        assertTrue(Greeting().greeting().contains("iOS"), "Check iOS is mentioned")
+    }
+}
