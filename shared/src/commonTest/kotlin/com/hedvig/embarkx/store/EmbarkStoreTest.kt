1129cf93874042a69a5b4c6d0c9b8cb48fc6ccd3
stylianosgakis <stylianos.gakis98@gmail.com>
2022-02-22 12:58:12 +0200

    Use @JsName to allow backticks and spaces in test names

--- shared/src/commonTest/kotlin/com/hedvig/embarkx/store/EmbarkStoreTest.kt
+++ shared/src/commonTest/kotlin/com/hedvig/embarkx/store/EmbarkStoreTest.kt
@@ -1,10 +1,12 @@
 package com.hedvig.embarkx.store
 
 import io.kotest.matchers.shouldBe
+import kotlin.js.JsName
 import kotlin.test.Test
 
 class EmbarkStoreTest {
     @Test
+    @JsName("should_be_able_to_put_a_key_then_get_it")
     fun `should be able to put a key then get it`() {
         val store = EmbarkStore()
 
@@ -13,12 +15,14 @@ class EmbarkStoreTest {
     }
 
     @Test
+    @JsName("should_return_null_when_getting_a_key_that_does_not_exist")
     fun `should return null when getting a key that does not exist`() {
         val store = EmbarkStore()
         store.get("foo") shouldBe null
     }
 
     @Test
+    @JsName("should_remove_values_when_rolling_back")
     fun `should remove values when rolling back`() {
         val store = EmbarkStore()
 
@@ -30,6 +34,7 @@ class EmbarkStoreTest {
     }
 
     @Test
+    @JsName("should_be_able_to_get_value_from_prefill_even_if_value_has_been_rolled_back")
     fun `should be able to get value from prefill even if value has been rolled back`() {
         val store = EmbarkStore()
 
--- shared/src/commonTest/kotlin/com/hedvig/embarkx/util/StackTest.kt
+++ shared/src/commonTest/kotlin/com/hedvig/embarkx/util/StackTest.kt
@@ -3,21 +3,25 @@ package com.hedvig.embarkx.util
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
 import kotlin.test.Test
+import kotlin.js.JsName
 
 class StackTest {
     @Test
+    @JsName("should_throw_when_popping_an_empty_stack")
     fun `should throw when popping an empty stack`() {
         val stack = Stack<Unit>()
         shouldThrow<EmptyStackException> { stack.pop() }
     }
 
     @Test
+    @JsName("should_throw_when_peeking_an_empty_stack")
     fun `should throw when peeking an empty stack`() {
         val stack = Stack<Unit>()
         shouldThrow<EmptyStackException> { stack.peek() }
     }
 
     @Test
+    @JsName("should_return_latest_pushed_value_when_peeking")
     fun `should return latest pushed value when peeking`() {
         val stack = Stack<String>()
 
@@ -27,6 +31,7 @@ class StackTest {
     }
 
     @Test
+    @JsName("should_return_latest_pushed_value_when_popping")
     fun `should return latest pushed value when popping`() {
         val stack = Stack<String>()
 
@@ -36,7 +41,8 @@ class StackTest {
     }
 
     @Test
-    fun `when multiple items are pushed, they are returned in order when popping them`() {
+    @JsName("when_multiple_items_are_pushed_they_are_returned_in_order_when_popping_them")
+    fun `when multiple items are pushed they are returned in order when popping them`() {
         val stack = Stack<String>()
 
         stack.push("foo")
