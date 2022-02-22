0171da828b039130aa8ce2e1d52be99407ba19c7
Sam Pettersson <sam@sampettersson.com>
2022-02-22 17:21:56 +0100

    Add ,

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "EmbarkX",
-            url: "https://github.com/HedvigInsurance/embark-x/releases/download/v0.0.11-swift-package.0/EmbarkX-unspecified.zip"
+            url: "https://github.com/HedvigInsurance/embark-x/releases/download/v0.0.11-swift-package.0/EmbarkX-unspecified.zip",
             checksum: "350e33d260adc134cf32e61bfeceadf7"
         ),
     ]
