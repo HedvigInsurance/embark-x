90da107d2898c9bfeb29b4964865f95b8acdb042
Guilherme Popolin <guilherme@hedvig.com>
2022-02-22 15:49:23 +0100

    added web sample

--- sample/web/src/index.ts
+++
@@ -1,7 +0,0 @@
-import * as shared from "@hedviginsurance/shared"
-
-shared.com.hedvig.embarkx.Greeting
-const embarkx = shared.com.hedvig.embarkx
-
-const greeting = new embarkx.Greeting().greeting()
-console.log(greeting)
