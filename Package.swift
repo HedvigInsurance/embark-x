a42d791d3964fc833b5eedd27ab499ec2e322cc3
Sam Pettersson <sam@sampettersson.com>
2022-02-22 10:46:12 +0100

    Swift Package 🤑

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "EmbarkX",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "EmbarkX",
+            targets: ["EmbarkX"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "EmbarkX",
+            url: "", // $XC_FRAMEWORK_URL
+            checksum: "" // $XC_FRAMEWORK_CHECKSUM
+        ),
+    ]
+)
