ff0f60309a00bfe4a8466881f092162750ec4300
tarikstafford <tarikstafford@gmail.com>
2022-02-23 14:17:06 +0100

    add ios proj sample

---
+++ sample/ios/ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by Tarik Stafford on 2022-02-23.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
