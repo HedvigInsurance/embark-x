014c59d8bcf60294b682bf2e4f081ee240b0101e
hedvigoscar <hedvigoscar@users.noreply.github.com>
2022-02-23 12:47:31 +0000

    Commit from GitHub Actions (Publish Swift Package)

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "EmbarkX",
-            url: "https://github.com/HedvigInsurance/embark-x/releases/download/v0.2.4/EmbarkX-v0.2.4.zip",
-            checksum: "311e9340d9a64aeaa3487efc08c431e25ec653e4397c45c3c3e53aa575908dd9"
+            url: "https://github.com/HedvigInsurance/embark-x/releases/download/v0.2.6/EmbarkX-v0.2.6.zip",
+            checksum: "54eb4469e610d33200eb5f1b7d3f45c45d9d22ab9b2d35a744e4cb7545a4abe1"
         ),
     ]
 )
